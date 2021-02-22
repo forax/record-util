4e0ebc8cbb3fbd9cb5f8f25c262c99b1f9633b5a
forax <forax@univ-mlv.fr>
2021-02-22 16:21:47 +0100

    add a small doc

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,10 @@
+/**
+ *  A module containing utility classes that sweeten the use of records.
+ *
+ * @see com.github.forax.recordutil.MapTrait
+ * @see com.github.forax.recordutil.WithTrait
+ * @see com.github.forax.recordutil.Wither
+ */
 module com.github.forax.recordutil {
-    exports com.github.forax.recordutil;
+  exports com.github.forax.recordutil;
 }
